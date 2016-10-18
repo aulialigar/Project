099a1b6c9fe51b3ca91f54173f464464d337bd43
Smktelkom <aulia_hanani_24rpl@student.smktelkom-mlg.sch.id>
2016-10-18 21:05:38 +0700

    hasil splashscreen

--- app/src/main/java/id/sch/smktelkom_mlg/learn/project/SplashScreen.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/project/SplashScreen.java
@@ -1,13 +1,40 @@
 package id.sch.smktelkom_mlg.learn.project;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
+
+    //set waktu splashscreen
+    private static int splashInterval = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.splashscreen);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         setContentView(R.layout.splashscreen);
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                
+                //jeda selesai Splashscreen
+                this.finish();
+            }
+
+            private void finish() {
+            }
+        }, splashInterval);
     }
 }
